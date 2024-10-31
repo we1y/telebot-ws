708213f01cd4b9d5958b94d43634dce3d76fe678
we1y <49814469+we1y@users.noreply.github.com>
2024-10-31 17:09:30 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-# Установка необходимых пакетов
 sudo apt-get update
-sudo apt-get install -y wget unzip xvfb libxi6 libgconf-2-4
+sudo apt-get install -y wget unzip xvfb libxi6 libgconf-2-4 \
+    libnss3 \
+    libnss3-tools \
+    libxss1 \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libgtk-3-0 \
+    libgdk-pixbuf2.0-0 \
+    libxcomposite1 \
+    libxcursor1 \
+    libxdamage1 \
+    libxfixes3 \
+    libxrandr2 \
+    libxtst6 \
+    libpango1.0-0 \
+    libgbm1
 
 # Установка Chrome
 wget https://storage.googleapis.com/chrome-for-testing-public/130.0.6723.91/linux64/chrome-linux64.zip
